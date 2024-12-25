44ffdb37711c95e1766e18fa994185c8be6b78fd
DESKTOP-CSBAJF8\WLOPEZOB <wlopezob@gmail.com>
2024-12-25 16:00:38 -0500

    changes

--- src/main/java/com/wlopezob/api_data_v1/ApiDataV1Application.java
+++ src/main/java/com/wlopezob/api_data_v1/ApiDataV1Application.java
@@ -3,10 +3,7 @@ package com.wlopezob.api_data_v1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-
 @SpringBootApplication
-@OpenAPIDefinition
 public class ApiDataV1Application {
 
 	public static void main(String[] args) {
